cd4bdc63bb07f253e7685b5085202d6ef8f633bc
Jack Chung <enepgoo@gmail.com>
2015-09-17 04:19:55 +0800

    added some comment

--- stackoverflow_fanatic/autologin.py
+++ stackoverflow_fanatic/autologin.py
@@ -12,23 +12,30 @@ loginUrl = 'https://stackoverflow.com/users/login?ssrc=head&returnurl=http%3a%2f
 username = 'iwillnotpostithere'
 password = 'noachance'
 
-   
-driver = webdriver.Firefox()
-driver.get(loginUrl)
-assert "Stack Overflow" in driver.title
+FFdriver = webdriver.Firefox()
+FFdriver.get(loginUrl)
+# should be "Log in - Stack Exchange - Stack Overflow"
+assert "Stack Overflow" in FFdriver.title
 
-emailInput = driver.find_element_by_name("email")
-passwordInput = driver.find_element_by_name("password")
+emailInput = FFdriver.find_element_by_name("email")
+passwordInput = FFdriver.find_element_by_name("password")
 
 emailInput.send_keys(username)
 passwordInput.send_keys(password)
 
 passwordInput.send_keys(Keys.RETURN)
 
-# wait until logo appear
-logo = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "hlogo")))
+# wait until logo 'stackoverflow' appear
+logo = WebDriverWait(FFdriver, 10).until(EC.presence_of_element_located((By.ID, "hlogo")))
 
-assert driver.current_url == 'http://stackoverflow.com/'
+# if successfully login, should be redirected to http://stackoverflow.com/
+assert FFdriver.current_url == 'http://stackoverflow.com/'
 print 'login success'
 
-driver.close()
+# not sure if pure login counts or need page reload, check user page 'last seen'
+# there's a delay between login and it refresh, just to ensure the page reload once after login in
+FFdriver.find_element_by_id('hlogo').click()
+# wait for reloading page..
+FFdriver.implicitly_wait(10)
+
+FFdriver.close()
